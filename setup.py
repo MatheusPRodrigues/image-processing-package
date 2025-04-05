d539cc01fae42433fe5db8e21a97f4d65a228fc4
MatheusPRodrigues <matherodrigues17@gmail.com>
2025-04-04 23:34:00 -0300

    Pacote postado no Pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="image_processing",
     version="0.0.1",
@@ -15,6 +12,10 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MatheusPRodrigues/image-processing-package",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scikit-image >= 0.16.1"
+    ],
     python_requires='>=3.5',
 )
